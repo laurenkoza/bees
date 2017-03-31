0b75a3d1008bc6026686f73f45c385c43b27800c
austinmitch <mitchellaustinj@gmail.com>
2017-03-31 15:03:26 -0400

    added login functionality for CMS system. created CMS form for updating services. no functionality on form yet

---
+++ app/Http/Controllers/CmsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use DB;
+
+class CmsController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+     public function getIndex()
+     {
+      $user = Auth::user();
+      $email = $user->email;
+      $password = $user->password;
+      return view('cms', ['email'=>$email, 'password'=>$password, 'title'=>'CMS']);
+     }
+
+     public function updateServices()
+     {
+       $serviceName = request()->input('serviceName');
+       $servicePrice = request()->input('servicePrice');
+       $serviceVisits = request()->input('serviceVisits');
+       $serviceDesc = request()->input('serviceDesc');
+       $serviceBenefits = request()->input('serviceBenefits');
+
+       
+
+     }
+}
---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Http\Request;
+use Response;
+use Illuminate\Support\Facades\Auth;
+
+use App\User;
+// use Validator;
+// use App\Http\Controllers\Controller;
+// use Illuminate\Foundation\Auth\ThrottlesLogins;
+// use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+
+class LoginController extends Controller
+{
+
+
+
+  // use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+      // $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+  public function testing()
+  {
+    $name = request()->input('name');
+    $email = request()->input('email');
+    $password = request()->input('password');
+    if(Auth::attempt(['email'=>$email, 'password'=>$password])){
+      // dd(Auth::user());
+      return redirect('cms');
+    }else{
+      return redirect('login');
+    }
+  }
+
+}
