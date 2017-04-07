e7d79bc6371ff5271ce14fb236e7e50623b5f4c1
austinmitch <mitchellaustinj@gmail.com>
2017-04-07 11:55:40 -0400

    split header parital into head and header so the cms can have the head elements without having the large splash screen

--- resources/views/cmsShop.blade.php
+++ resources/views/cmsShop.blade.php
@@ -1,16 +1,18 @@
-<!doctype html>
+<!-- <!doctype html>
 <html class="no-js" lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title; ?></title>
+    <title></title>
     <link rel="stylesheet" href="../css/foundation.css">
     <link rel="stylesheet" href="../css/app.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,700,900|Open+Sans:300,300i,400,700" rel="stylesheet">
   </head>
 
-<body>
+<body> -->
+@include('layouts.head')
+@include('layouts.cmsNav')
   <section class="mainContent">
 
   <h2 class="cmsHeading">CONTENT MANAGMENT</h2>
--- resources/views/layouts/headerHome.blade.php
+++ resources/views/layouts/headerHome.blade.php
@@ -1,16 +1,4 @@
-<!doctype html>
-<html class="no-js" lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title; ?></title>
-    <link rel="stylesheet" href="css/foundation.css">
-    <link rel="stylesheet" href="css/app.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,700,900|Open+Sans:300,300i,400,700" rel="stylesheet">
-  </head>
 
-<body>
   <header id="{{$headerImg}}" class="expanded row">
     <div class="layer" data-depth="-0.15"><img src="img/splashImg/{{$headerImg}}.jpg" class="parallaxImg" alt=""></div>
     <div class="layer hex" data-depth="0.20"><!-- parallax layer --></div>
